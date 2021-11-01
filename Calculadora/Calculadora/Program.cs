19fb66d5e772ff275965c5d284f5ad7b153013d2
HitaloAlves <31925493+HitaloAlves@users.noreply.github.com>
2021-11-01 19:12:05 -0300

    Exercícios da Lista de Convidados e Calculadora Simples

---
+++ Calculadora/Calculadora/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static float RealizarOperacao(float primeiroNum, float segundoNum, string operador)
+        {
+            switch (operador)
+            {
+                case "+": return primeiroNum + segundoNum;
+                case "-": return primeiroNum - segundoNum;
+                case "*": return primeiroNum * segundoNum;
+                case "/": return primeiroNum / segundoNum;
+                case "%": return primeiroNum % segundoNum;
+                default: return 0f;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe um numero: ");
+            var primeiroNumString = Console.ReadLine();
+            float primeiroNum;
+
+            bool isPrimeiroNumValido = float.TryParse(primeiroNumString, out primeiroNum);
+
+            if (!isPrimeiroNumValido)
+            {
+                Console.WriteLine("Primeiro Número Invalido");
+                return;
+            }
+
+            Console.WriteLine("Informe outro numero: ");
+            var segundoNumString = Console.ReadLine();
+            float segundoNum;
+
+            bool isSegundoNumValido = float.TryParse(segundoNumString, out segundoNum);
+
+            if (!isSegundoNumValido)
+            {
+                Console.WriteLine("Segundo Número Invalido");
+                return;
+            }
+
+            Console.WriteLine("Informe o operador: ");
+            var operador = Console.ReadLine();
+
+            var resultado = RealizarOperacao(primeiroNum, segundoNum, operador);
+            Console.WriteLine("O resultado é: " + resultado);
+        }
+    }
+}
---
+++ ListaDeConvidados/ListaDeConvidados/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace ListaDeConvidados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o nome do convidado: ");
+            var nome = Console.ReadLine();
+
+            if(string.IsNullOrWhiteSpace(nome) || string.IsNullOrEmpty(nome))
+            {
+                Console.WriteLine("O nome não foi informado");
+                return;
+            }
+
+            Console.WriteLine("Informe a idade do convidado: ");
+
+            var idadeString = Console.ReadLine();
+            int idade;
+
+            bool idadeInformada = int.TryParse(idadeString, out idade);
+            if(idadeInformada == false)
+            {
+                Console.WriteLine("Idade informada não é valida");
+                return;
+            }
+
+            bool estaConvidado;
+            switch (nome)
+            {
+                case "Hitalo":
+                    estaConvidado = true;
+                    break;
+                case "Daniel":
+                        estaConvidado = true;
+                    break;
+                case "Jose":
+                    estaConvidado = true;
+                    break;
+                case "Mateus":
+                        estaConvidado = true;
+                    break;
+                case "Bento":
+                        estaConvidado = true;
+                    break;
+                case "Gabriela":
+                        estaConvidado = true;
+                    break;
+                case "Fernanda":
+                    estaConvidado = true;
+                    break;
+                case "Bianca":
+                    estaConvidado = true;
+                    break;
+                default:
+                    estaConvidado = false;
+                    break;
+            }
+
+            if(estaConvidado == true && idade > 18)
+            {
+                Console.WriteLine("Seja Bem vindo(a) a festa !");
+            }else if(estaConvidado == false)
+            {
+                Console.WriteLine("O seu nome não esta na lista, por favor, verifique com a pessoa que te convidou");
+            }
+            else
+            {
+                Console.WriteLine("Você não tem 18 anos para entrar na festa");
+            }
+        }
+    }
+}
