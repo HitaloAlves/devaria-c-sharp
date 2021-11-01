55d33fdc1cd5b6fcd53035c222b3c7b59107c5e1
HitaloAlves <31925493+HitaloAlves@users.noreply.github.com>
2021-11-01 17:44:40 -0300

    Meu primeiro commit de C#

---
+++ TesteVarConst/TesteVarConst/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TesteVarConst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string constante = "uma constate que não muda";
+
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Por favor, informe um número");
+                return;
+            }
+
+            int inteiro;
+            bool teste = int.TryParse(args[0], out inteiro);
+
+            if (teste == false)
+            {
+                Console.WriteLine("O argumento informado não é inteiro");
+                return;
+            }
+
+            Console.WriteLine(constante);
+            Console.WriteLine("O número informado foi: " + inteiro);
+        }
+    }
+}
